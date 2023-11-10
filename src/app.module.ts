546ac60e4b34d9e2f5565a79db056f28c70ce33f
seo <emma05127@gmail.com>
2023-11-10 17:47:22 +0900

    ✨ Add Cloudtype settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { OpenGraphModule } from 'src/open-graph/open-graph.module';
 
 @Module({
-  imports: [OpenGraphModule],
+  imports: [
+    OpenGraphModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
