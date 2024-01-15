82d24370f00370e17016f6bf316ce1710137c04e
seo <emma05127@gmail.com>
2024-01-15 15:54:24 +0900

    ✨ Add view increment api controller with session
    - 세션과 postId를 이용한 조회수 체크 로직 구현
    - 30분 내 같은 포스트 방문 시 조회수 카운트 하지 않도록 구현

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Patch, Get, Body } from '@nestjs/common';
+import { Controller, Patch, Get, Body, Post, Session } from '@nestjs/common';
 import { PostsService } from 'src/post/post.service';
+import type { SessionViews } from 'src/post/types';
 
 @Controller('post')
 export class PostsController {
@@ -10,8 +11,39 @@ export class PostsController {
     return this.postsService.findAll();
   }
 
+  @Post('/views/session')
+  setSession(
+    @Body('id') postId: string,
+    @Session() session: Record<string, any>,
+  ) {
+    session[postId] = true;
+  }
+
+  @Get('/views/session/:id')
+  getSession(@Session() session: Record<string, any>) {
+    console.log(session);
+  }
+
   @Patch('/views/increment')
-  increment(@Body('id') id: string) {
-    return this.postsService.increment(id);
+  increment(
+    @Body('id') postId: string,
+    @Session() session: Record<string, any> & SessionViews,
+  ) {
+    if (!session.views) {
+      session.views = {};
+    }
+    const thirtyMinutes = 30 * 60 * 1000;
+    const lastViewTime = session.views[postId] || 0;
+
+    if (Date.now() - lastViewTime > thirtyMinutes) {
+      this.postsService.incrementViewCount(postId);
+      session.views[postId] = Date.now();
+      return { status: true, message: '' };
+    }
+
+    return {
+      status: false,
+      message: '⚠️ 30분 내의 이전 방문 기록이 존재합니다.',
+    };
   }
 }
