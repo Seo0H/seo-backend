76b85bc0a446c633d5b1f8799da6d2e26e67343c
seo <seo0h9@gmail.com>
2024-02-13 19:24:18 +0900

    Fix import entity

--- src/lib/config/db.ts
+++ src/lib/config/db.ts
@@ -2,21 +2,25 @@ import { Logger } from '@nestjs/common';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { isDev } from 'src/lib/config/mode';
 
-export default function typeOrmConfig() {
+export default function typeOrmConfig(): TypeOrmModuleOptions {
   const logger = new Logger(typeOrmConfig.name);
 
+  let config: TypeOrmModuleOptions = {
+    url: process.env.DB_URL,
+    type: 'postgres',
+    logging: true,
+    synchronize: true,
+    ssl: {
+      ca: process.env.DB_SSL_PATH,
+      rejectUnauthorized: false,
+    },
+    autoLoadEntities: true,
+  };
+
   if (isDev) {
-    const config: TypeOrmModuleOptions = {
-      url: process.env.DB_URL,
-      type: 'postgres',
-      logging: true,
-      synchronize: true,
+    config = {
+      ...config,
       entities: [__dirname + '/entities/**/*.dev.entity.ts'],
-      ssl: {
-        ca: process.env.DB_SSL_PATH,
-        rejectUnauthorized: false,
-      },
-      autoLoadEntities: true,
     };
 
     logger.log('TypeOrm Dev mode is running.');
@@ -24,17 +28,9 @@ export default function typeOrmConfig() {
     return config;
   }
 
-  const config: TypeOrmModuleOptions = {
-    url: process.env.DB_URL,
-    type: 'postgres',
-    logging: true,
-    synchronize: false,
+  config = {
+    ...config,
     entities: [__dirname + '/entities/**/*.entity.ts'],
-    ssl: {
-      ca: process.env.DB_SSL_PATH,
-      rejectUnauthorized: false,
-    },
-    autoLoadEntities: true,
   };
 
   logger.log('TypeOrm Prod mode is running.');
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,12 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PostsService } from 'src/post/post.service';
 import { PostsController } from 'src/post/post.controller';
 import { isDev } from 'src/lib/config/mode';
+
 import DevPost from 'src/entities/post/index.dev.entity';
+import Post from 'src/entities/post/index.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature(isDev ? [DevPost] : [Post])],
